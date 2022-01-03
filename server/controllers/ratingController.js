4dff687553e77fdd50545934aafedd3064ae585c
Shtymak <vova.shtimak@gmail.com>
2022-01-03 16:46:07 +0200

    Added ratingController without logic.

---
+++ server/controllers/ratingController.js
@@ -0,0 +1,13 @@
+const ApiError = require('../error/ApiError')
+const {Device, User, Rating} = require('../models/models')
+class RatingController{
+    update(req, res, next){
+        //Todo: write a logic
+    }
+
+    getOne(req, res, next){
+        //Todo: write a logic
+    }
+}
+
+module.exports = new RatingController()
