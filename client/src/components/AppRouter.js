0c6faf6becc65089db1bba9b2783932f7985315a
Shtymak <vova.shtimak@gmail.com>
2022-01-03 19:13:50 +0200

    Added routes

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Switch, Redirect, Route} from "react-router-dom";
+
+const AppRouter = () => {
+    const isAuth = true
+    return (
+        <Switch>
+
+        </Switch>
+    );
+};
+
+export default AppRouter;
