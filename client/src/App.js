5c5795ed32ae5ce43ae35b2cd13fe09e49154997
Shtymak <vova.shtimak@gmail.com>
2022-01-03 19:13:50 +0200

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 function App() {
-  return (
-    <div>
-        h
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <AppRouter/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
