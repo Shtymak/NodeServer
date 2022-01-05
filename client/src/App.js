a65e216c8a8f24d32bc896f1b534b73e88202a5b
Shtymak <vova.shtimak@gmail.com>
2022-01-05 20:46:49 +0200

    Added NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
 
 function App() {
     return (
         <BrowserRouter>
+            <NavBar/>
             <AppRouter/>
         </BrowserRouter>
     );
