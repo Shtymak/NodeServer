6b35ad19f9d0413a1f2a10dacd0de112b2d21b1b
Shtymak <vova.shtimak@gmail.com>
2022-01-05 20:13:47 +0200

    Added device store for device get/set control

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,15 @@ import React, {useContext} from 'react';
 import ReactDOM from 'react-dom';
 import App from "../src/App"
 import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
 export const Context = useContext(null)
 
 
 ReactDOM.render(
     <Context.Provider value={{
-        user: new UserStore()
+        user: new UserStore(),
+        device: new DeviceStore()
     }}>
         <App/>
     </Context.Provider>,
