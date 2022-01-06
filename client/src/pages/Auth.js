df27809db19ff4dc2093359d30e0430c96508040
Shtymak <vova.shtimak@gmail.com>
2022-01-06 21:00:10 +0200

    Added Auth page [0.1]

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Button, Card, Container, Form} from "react-bootstrap";
+import classes from "../modules/Auth.module.css"
 
 const Auth = () => {
     return (
-        <div>
-            Auth
-        </div>
+        <Container className="justify-content-center align-items-center d-flex"
+                   style={{height: window.innerHeight - 54}}>
+            <Card className="p-5"
+                  style={{width: 600}}>
+                <h2 className="m-auto">Авторизація</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control className="mt-2" placeholder="Email"/>
+                    <Form.Control className="mt-2" placeholder="Пароль"/>
+                    <Button className="mt-2">Ввійти</Button>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
