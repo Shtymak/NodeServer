619dc6859596a38b35f1a6084feeb10b0f19fb44
Shtymak <vova.shtimak@gmail.com>
2022-01-07 18:33:52 +0200

    Fix brand bar. Replaced list group to row-card system

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {ListGroup} from "react-bootstrap";
+import {Card, ListGroup, Row} from "react-bootstrap";
 import {Context} from "../index";
 import {observer} from "mobx-react-lite";
 import classes from "../modules/BrandBar.module.css";
@@ -7,20 +7,18 @@ import classes from "../modules/BrandBar.module.css";
 const BrandBar = observer(() => {
     const {device} = useContext(Context)
     return (
-        <ListGroup horizontal>
-            {
-                device.brands.map(brand =>
-                    <ListGroup.Item
-                        onClick={() => device.setSelectedBrand(brand)}
-                        active={brand.id === device.selectedBrand.id}
-                        key={brand.id}
-                        className={classes._item}
-                    >
-                        {brand.name}
-                    </ListGroup.Item>)
-            }
-
-        </ListGroup>
+        <Row>
+            {device.brands.map(brand =>
+                <Card
+                    className={classes._item}
+                    key={brand.id}
+                    onClick={() => device.setSelectedBrand(brand)}
+                    border={brand.id === device.selectedBrand.id ? 'danger' : 'light'}
+                >
+                    {brand.name}
+                </Card>
+            )}
+        </Row>
     );
 });
 
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -8,7 +8,14 @@ export default class DeviceStore {
         ]
         this._brands = [
             {id: 1, name: "Brand#1"},
-            {id: 2, name: "Brand#2"}
+            {id: 2, name: "Brand#2"},
+            {id: 3, name: "Brand#3"},
+            {id: 4, name: "Brand#4"},
+            {id: 5, name: "Brand#5"},
+            {id: 6, name: "Brand#6"},
+            {id: 7, name: "Brand#7"},
+            {id: 8, name: "Brand#8"},
+            {id: 9, name: "Brand#9"},
         ]
         this._devices = [
             {id: 1, name: "12 pro", price: 12000, rating: 5, img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000"}
