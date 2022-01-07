65118e1f4325bb5666d04f2a7aea091341c4b63b
Shtymak <vova.shtimak@gmail.com>
2022-01-07 17:57:59 +0200

    Refactor type bar. Added logic to selected type and user-friendly interface

--- client/src/components/TypeBar.js
+++ client/src/components/TypeBar.js
@@ -1,14 +1,20 @@
 import React, {useContext} from 'react';
 import {observer} from "mobx-react-lite";
 import {Context} from "../index";
-import {ListGroup, ListGroupItem} from "react-bootstrap";
+import classes from "../modules/TypeBar.module.css"
+import {ListGroup} from "react-bootstrap";
 
 const TypeBar = observer(() => {
     const {device} = useContext(Context)
     return (
         <ListGroup as="ul">
             {device.types.map(type =>
-                <ListGroup.Item key={type.id}>
+                <ListGroup.Item
+                    className={classes._item}
+                    active={device.selectedType.id === type.id}
+                    onClick={() => device.setSelectedType(type)}
+                    key={type.id}
+                >
                     {type.name}
                 </ListGroup.Item>)}
         </ListGroup>
