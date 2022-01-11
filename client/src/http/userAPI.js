669ba8b4a83c3924ba56313e6310bc5cf656d420
Shtymak <vova.shtimak@gmail.com>
2022-01-11 23:17:00 +0200

    [REFACTOR] set token to local storage and rewrite token. Added check login logic

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -1,12 +1,14 @@
-import {$host} from "./index";
+import {$authHost, $host} from "./index";
 import jwt_decode from "jwt-decode";
 
+const tokenKey = 'token'
 export const registration = async (email, password) => {
     const {data} = await $host.post('api/user/registration', {
         email,
         password,
         role: "USER"
     })
+    localStorage.setItem(tokenKey, data.token)
     return jwt_decode(data.token)
 }
 
@@ -15,8 +17,13 @@ export const login = async (email, password) => {
         email,
         password
     })
+    localStorage.setItem(tokenKey, data.token)
     return jwt_decode(data.token)
 }
 
-export const check = async () => await $host.post('api/auth')
+export const check = async () => {
+    const {data} = await $authHost.get('api/user/auth')
+    localStorage.setItem(tokenKey, data.token)
+    return jwt_decode(data.token)
+}
 
