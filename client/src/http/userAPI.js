710576bf892ab3b268118828b29911ecc3c3c7e1
Shtymak <vova.shtimak@gmail.com>
2022-01-11 22:57:45 +0200

    [REFACTOR] replace useApi path and added jwt

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -1,8 +1,22 @@
 import {$host} from "./index";
+import jwt_decode from "jwt-decode";
 
-export const registration = async (email, password) => await $host.post('api/user/registration', {email, password, role: "USER"})
+export const registration = async (email, password) => {
+    const {data} = await $host.post('api/user/registration', {
+        email,
+        password,
+        role: "USER"
+    })
+    return jwt_decode(data.token)
+}
 
-export const login = async (email, password) => await $host.post('api/user/login', {email, password})
+export const login = async (email, password) => {
+    const {data} = await $host.post('api/user/login', {
+        email,
+        password
+    })
+    return jwt_decode(data.token)
+}
 
 export const check = async () => await $host.post('api/auth')
 
