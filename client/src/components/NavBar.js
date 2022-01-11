c28058a5d852c46f9ba224853f33e4bd74ec5f9d
Shtymak <vova.shtimak@gmail.com>
2022-01-11 21:45:51 +0200

    Added interceptors. Relationships front + beck

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,8 +4,10 @@ import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
 import {SHOP_ROUTE} from "../utils/consts";
 import classes from "../modules/NavBar.module.css"
 import {observer} from "mobx-react-lite";
+import {useHistory} from "react-router-dom";
 
 const NavBar = observer(() => {
+    const history = useHistory()
     const {user} = useContext(Context)
     return (
         <Navbar bg="dark" variant="dark">
