47bc0444e49f921ba12ba856f9db6e5a3abce681
Shtymak <vova.shtimak@gmail.com>
2022-01-11 21:26:53 +0200

    Added device page(alpha)

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import star from "../assets/star.png"
 
 const DevicePage = () => {
+    const device = {
+        id: 3,
+        name: "12 pro",
+        price: 12000,
+        rating: 5,
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000"
+    }
+    const descriptions = []
     return (
-        <div>
-            Device
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{
+                                background: `url(${star}) no-repeat center center`,
+                                width: 240,
+                                height: 240,
+                                backgroundSize: 'cover',
+                                fontSize: 64
+                            }}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {descriptions.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
