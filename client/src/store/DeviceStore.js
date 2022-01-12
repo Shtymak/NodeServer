31f723fc3b21903f89ac9e5eb512b13b7fb6898a
Shtymak <vova.shtimak@gmail.com>
2022-01-12 22:24:13 +0200

    [NEW] Load brands to type bar from server

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -3,17 +3,7 @@ import {makeAutoObservable} from "mobx";
 export default class DeviceStore {
     constructor() {
         this._types = []
-        this._brands = [
-            {id: 1, name: "Brand#1"},
-            {id: 2, name: "Brand#2"},
-            {id: 3, name: "Brand#3"},
-            {id: 4, name: "Brand#4"},
-            {id: 5, name: "Brand#5"},
-            {id: 6, name: "Brand#6"},
-            {id: 7, name: "Brand#7"},
-            {id: 8, name: "Brand#8"},
-            {id: 9, name: "Brand#9"},
-        ]
+        this._brands = []
         this._devices = [
             {
                 id: 1,
