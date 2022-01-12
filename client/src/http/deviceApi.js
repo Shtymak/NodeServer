1ea548478bf58f378abadf91f99766b24009973a
Shtymak <vova.shtimak@gmail.com>
2022-01-12 22:16:54 +0200

    [NEW] Load types from server to typebar

--- client/src/http/deviceApi.js
+++ client/src/http/deviceApi.js
@@ -3,7 +3,9 @@ import {$authHost, $host} from "./index";
 export const createType = async (type) =>
     await $authHost.post('api/type', type) //TODO: think about permission auth or !auth
 
-export const fetchType = async () =>
-    await $host.get('api/type')
+export const fetchType = async () =>{
+    const {data} = await $host.get('api/type')
+    return data
+}
 
 
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import TypeBar from "../components/TypeBar";
 import BrandBar from "../components/BrandBar";
 import DeviceList from "../components/DeviceList";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {fetchType} from "../http/deviceApi";
 
-const Shop = () => {
+const Shop = observer(() => {
+    const {device} = useContext(Context)
+    const loadTypes = () => {
+        fetchType().then(data => device.setTypes(data))
+    }
+    useEffect(() => fetchType().then(data => device.setTypes(data)), [])
     return (
         <Container>
             <Row className='mt-2'>
@@ -18,6 +26,6 @@ const Shop = () => {
             </Row>
         </Container>
     );
-};
+});
 
 export default Shop;
