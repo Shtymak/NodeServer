7ab86d1710d3bade289552b1b275e9a6b8801566
Shtymak <vova.shtimak@gmail.com>
2022-01-12 22:02:24 +0200

    [REALISE] Added tools to CRUD product

--- client/src/http/deviceApi.js
+++ client/src/http/deviceApi.js
@@ -1,5 +1,9 @@
 import {$authHost, $host} from "./index";
 
-const createType = async () => {
+export const createType = async (type) =>
+    await $authHost.post('api/type', type) //TODO: think about permission auth or !auth
 
-}
+export const fetchType = async () =>
+    await $host.get('api/type')
+
+
