d9a2f9be7ed03c76fc8adcc6b3370f1b1608839e
Shtymak <vova.shtimak@gmail.com>
2022-01-14 00:10:33 +0200

    [REFACTOR] Type & brand filter in SHOP

--- client/src/http/deviceApi.js
+++ client/src/http/deviceApi.js
@@ -18,8 +18,8 @@ export const fetchBrand = async () => {
 export const createDevice = async (device) =>
     await $authHost.post('api/device', device) //TODO: think about permission auth or !auth
 
-export const fetchDevice = async () => {
-    const {data} = await $host.get('api/device')
+export const fetchDevice = async (brandId, typeId) => {
+    const {data} = await $host.get('api/device',{params: {brandId, typeId}})
     return data
 }
 
