4893611bdd829f16da00dad6c852b67c97e1cc74
Shtymak <vova.shtimak@gmail.com>
2022-01-20 21:28:42 +0200

    [REFACTOR] Deleted unnecessary properties to basket request

--- server/controllers/basketController.js
+++ server/controllers/basketController.js
@@ -5,23 +5,24 @@ const ApiError = require('../error/ApiError')
 class BasketController {
 
     async getAndCountAll(req, res) {
-        const {basketId} = req.body
-        const basket = await Basket.findOne({where: {id: basketId}})
+        const basket = await Basket.findOne({where: {userId: req.user.id}});
         const devices = await BasketDevice.findAndCountAll({where: {basketId: basket.id}})
         return res.json({devices})
     }
 
     async addToBasket(req, res, next) {
-        const {deviceId, basketId} = req.body
-        let device = await BasketDevice.create({deviceId: deviceId, basketId: basketId})
+        const {id} = await Basket.findOne({where: {userId: req.user.id}});
+        const {deviceId} = req.body
+        let device = await BasketDevice.create({deviceId: deviceId, basketId: id})
         if (!device)
             return next(ApiError.BadRequest("Помилка. Неможливо додати!"))
         res.json({device})
     }
 
     async deleteFromBasket(req, res, next) {
-        const {deviceId, basketId} = req.body
-        let device = await BasketDevice.destroy({where: {deviceId: deviceId, basketId: basketId}})
+        const {deviceId} = req.body
+        const {id} = await Basket.findOne({where: {userId: req.user.id}});
+        let device = await BasketDevice.destroy({where: {deviceId: deviceId, basketId: id}})
         if (!device)
             return next(ApiError.BadRequest("Помилка. Неможливо видалити!"))
         res.json({message: "Продукт успішно видлалено!"})
