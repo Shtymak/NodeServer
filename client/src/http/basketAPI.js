7ad2f5e58e4e2fe6f559d65e0c32925bf4f7ab00
Shtymak <vova.shtimak@gmail.com>
2022-01-21 22:38:41 +0200

    [FIX] Logout

--- client/src/http/basketAPI.js
+++ client/src/http/basketAPI.js
@@ -1,15 +1,17 @@
 import {$authHost} from "./index";
 
+const basketPath = "api/basket"
+
 export const addToBasket = async (deviceId) => {
-    await $authHost.post('api/basket', {deviceId})
+    await $authHost.post(basketPath, {deviceId})
 }
 
 export const fetchBasket = async () => {
-    const {data} = await $authHost.get('api/basket')
+    const {data} = await $authHost.get(basketPath)
     return data
 }
 
 
 export const deleteFromBasket = async (deviceId) => {
-    await $authHost.delete('api/basket', {data: deviceId})
+    await $authHost.delete(basketPath, {data: deviceId})
 }
