fa703681355d1418e985c9ae9315edfedd3e9c4c
Shtymak <vova.shtimak@gmail.com>
2022-01-21 19:24:43 +0200

    [NEW] Updated device deleting

--- client/src/components/BasketList.js
+++ client/src/components/BasketList.js
@@ -6,10 +6,12 @@ import {observer} from "mobx-react-lite";
 
 const BasketList = observer(() => {
     const {basket} = useContext(Context)
+    const deleteBasketDevice = (id) =>
+        basket.setBasketDevices(basket.basketDevices.filter(x => x.id !== id))
     return (
         <Row>
             {basket.basketDevices.map(device =>
-                <BasketItem device={device} key={device.id}/>
+                <BasketItem device={device} deleteFromBasketList={deleteBasketDevice} key={device.id}/>
             )}
         </Row>
     );
