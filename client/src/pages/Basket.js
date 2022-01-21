4b07a8ab1371de3a94ad7e9d4334b4b951ed86df
Shtymak <vova.shtimak@gmail.com>
2022-01-21 17:39:15 +0200

    [NEW] Load devices to client and delete from basket

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,14 +1,19 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect} from 'react';
 import BasketList from "../components/BasketList";
 import {fetchBasket} from "../http/basketAPI";
 import {login} from "../http/userAPI";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
 
-const Basket = () => {
+const Basket = observer(() => {
+    const {basket} = useContext(Context)
+    const loadBasket = () => fetchBasket().then(data=>basket.setBasketDevices(data.devices))
+    useEffect(loadBasket,[])
     return (
         <div>
             <BasketList/>
         </div>
     );
-};
+});
 
 export default Basket;
