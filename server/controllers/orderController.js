efc306ea09216ad571675cb4fbbac575b854fe91
Shtymak <vova.shtimak@gmail.com>
2022-02-26 21:08:48 +0200

    [NEW] Created router for order

---
+++ server/controllers/orderController.js
@@ -0,0 +1,19 @@
+class OrderController {
+    async create(req, res, next) {
+
+    }
+
+    async destroy(req, res, next) {
+
+    }
+
+    async getOne(req, res, next) {
+
+    }
+
+    async getAll(req, res, next) {
+        
+    }
+}
+
+module.exports = new OrderController();
---
+++ server/routes/orderRouter.js
@@ -0,0 +1,5 @@
+const authMiddleware = require('../middleware/authMiddleware')
+const Router = require("express")
+const router = new Router()
+
+const orderController = require('../controllers/orderController')
